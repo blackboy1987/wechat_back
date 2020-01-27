20361000e96886aac6b289ea9506499befa23524
xiali <A12345678>
2020-01-27 18:02:10 +0800

    init

--- src/main/java/com/igomall/controller/admin/member/MemberController.java
+++ src/main/java/com/igomall/controller/admin/member/MemberController.java
@@ -1,7 +1,8 @@
 
-package com.igomall.controller.common.setting;
+package com.igomall.controller.admin.member;
 
 import com.igomall.common.Message;
+import com.igomall.common.Page;
 import com.igomall.common.Pageable;
 import com.igomall.controller.admin.BaseController;
 import com.igomall.entity.BaseEntity;
@@ -14,8 +15,10 @@ import com.igomall.service.member.MemberService;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
@@ -23,11 +26,11 @@ import java.math.BigDecimal;
 /**
  * Controller - 会员
  * 
- * @author IGOMALL  Team
+ * @author blackboy
  * @version 1.0
  */
-@RestController("commonMemberController")
-@RequestMapping("/api/member")
+@RestController("adminMemberController")
+@RequestMapping("/admin/api/member")
 public class MemberController extends BaseController {
 
 	@Autowired
@@ -42,134 +45,104 @@ public class MemberController extends BaseController {
 	/**
 	 * 检查用户名是否存在
 	 */
-	@GetMapping("/check_username")
-	public @ResponseBody boolean checkUsername(String username) {
+	@PostMapping("/check_username")
+	public boolean checkUsername(String username) {
 		return StringUtils.isNotEmpty(username) && !memberService.usernameExists(username);
 	}
 
 	/**
 	 * 检查E-mail是否唯一
 	 */
-	@GetMapping("/check_email")
-	public @ResponseBody boolean checkEmail(Long id, String email) {
+	@PostMapping("/check_email")
+	public boolean checkEmail(Long id, String email) {
 		return StringUtils.isNotEmpty(email) && memberService.emailUnique(id, email);
 	}
 
 	/**
 	 * 检查手机是否唯一
 	 */
-	@GetMapping("/check_mobile")
-	public @ResponseBody boolean checkMobile(Long id, String mobile) {
+	@PostMapping("/check_mobile")
+	public boolean checkMobile(Long id, String mobile) {
 		return StringUtils.isNotEmpty(mobile) && memberService.mobileUnique(id, mobile);
 	}
 
 	/**
 	 * 查看
 	 */
-	@GetMapping("/view")
-	public String view(Long id, ModelMap model) {
-		Member member = memberService.find(id);
-		model.addAttribute("genders", Member.Gender.values());
-		model.addAttribute("memberAttributes", memberAttributeService.findList(true, true));
-		model.addAttribute("member", member);
-		return "admin/member/view";
-	}
-
-	/**
-	 * 添加
-	 */
-	@GetMapping("/add")
-	public String add(ModelMap model) {
-		model.addAttribute("genders", Member.Gender.values());
-		model.addAttribute("memberRanks", memberRankService.findAll());
-		model.addAttribute("memberAttributes", memberAttributeService.findList(true, true));
-		return "admin/member/add";
+	@PostMapping("/view")
+	public Member view(Long id) {
+		return memberService.find(id);
 	}
 
 	/**
 	 * 保存
 	 */
 	@PostMapping("/save")
-	public String save(Member member, Long memberRankId, HttpServletRequest request) {
+	public Message save(Member member, Long memberRankId, HttpServletRequest request) {
 		member.init();
-		if(memberRankId!=null){
-			member.setMemberRank(memberRankService.find(memberRankId));
-		}else {
-			member.setMemberRank(memberRankService.findDefault());
-		}
-		if(StringUtils.isEmpty(member.getPassword())){
-			member.setPassword("123456");
-		}
-		if(StringUtils.isEmpty(member.getEmail())){
-			member.setEmail(member.getUsername()+"@i-gomall.com");
-		}
-
-
+		member.setEmail(member.getUsername()+"@i-gomall.com");
+		member.setMemberRank(memberRankService.find(memberRankId));
 		if (!isValid(member, BaseEntity.Save.class)) {
-			return ERROR_VIEW;
+			return Message.error("参数错误");
 		}
 		if (memberService.usernameExists(member.getUsername())) {
-			return ERROR_VIEW;
+			return Message.error("用户名已存在");
 		}
 		if (memberService.emailExists(member.getEmail())) {
-			return ERROR_VIEW;
+			return Message.error("邮箱已存在");
 		}
 		if (StringUtils.isNotEmpty(member.getMobile()) && memberService.mobileExists(member.getMobile())) {
-			return ERROR_VIEW;
+			return Message.error("手机号已存在");
 		}
 		member.removeAttributeValue();
 		for (MemberAttribute memberAttribute : memberAttributeService.findList(true, true)) {
 			String[] values = request.getParameterValues("memberAttribute_" + memberAttribute.getId());
 			if (!memberAttributeService.isValid(memberAttribute, values)) {
-				return ERROR_VIEW;
+				return Message.error("参数错误");
 			}
 			Object memberAttributeValue = memberAttributeService.toMemberAttributeValue(memberAttribute, values);
 			member.setAttributeValue(memberAttribute, memberAttributeValue);
 		}
 		memberService.save(member);
-		return "redirect:list";
+		return Message.success("操作成功");
 	}
 
 	/**
 	 * 编辑
 	 */
-	@GetMapping("/edit")
-	public String edit(Long id, ModelMap model) {
-		Member member = memberService.find(id);
-		model.addAttribute("genders", Member.Gender.values());
-		model.addAttribute("memberRanks", memberRankService.findAll());
-		model.addAttribute("memberAttributes", memberAttributeService.findList(true, true));
-		model.addAttribute("member", member);
-		return "admin/member/edit";
+	@PostMapping("/edit")
+	public Member edit(Long id) {
+		return memberService.find(id);
 	}
 
 	/**
 	 * 更新
 	 */
 	@PostMapping("/update")
-	public String update(Member member, Long id, Long memberRankId, Boolean unlock, HttpServletRequest request) {
+	public Message update(Member member, Long id, Long memberRankId, Boolean unlock, HttpServletRequest request) {
 		member.setMemberRank(memberRankService.find(memberRankId));
+		member.setEmail(member.getUsername()+"@i-gomall.com");
 		if (!isValid(member)) {
-			return ERROR_VIEW;
+			return Message.error("参数错误");
 		}
 		if (!memberService.emailUnique(id, member.getEmail())) {
-			return ERROR_VIEW;
+			return Message.error("邮箱已存在");
 		}
 		if (StringUtils.isNotEmpty(member.getMobile()) && !memberService.mobileUnique(id, member.getMobile())) {
-			return ERROR_VIEW;
+			return Message.error("手机号已存在");
 		}
 		member.removeAttributeValue();
 		for (MemberAttribute memberAttribute : memberAttributeService.findList(true, true)) {
 			String[] values = request.getParameterValues("memberAttribute_" + memberAttribute.getId());
 			if (!memberAttributeService.isValid(memberAttribute, values)) {
-				return ERROR_VIEW;
+				return Message.error("参数错误");
 			}
 			Object memberAttributeValue = memberAttributeService.toMemberAttributeValue(memberAttribute, values);
 			member.setAttributeValue(memberAttribute, memberAttributeValue);
 		}
 		Member pMember = memberService.find(id);
 		if (pMember == null) {
-			return ERROR_VIEW;
+			return Message.error("参数错误");
 		}
 		if (BooleanUtils.isTrue(pMember.getIsLocked()) && BooleanUtils.isTrue(unlock)) {
 			userService.unlock(member);
@@ -179,25 +152,22 @@ public class MemberController extends BaseController {
 			memberService.update(member, "username", "encodedPassword", "point", "balance", "amount", "isLocked", "lockDate", "lastLoginIp", "lastLoginDate", "loginPluginId", "safeKey", "cart", "orders", "socialUsers", "paymentTransactions", "memberDepositLogs", "couponCodes", "receivers",
 					"reviews", "consultations", "productFavorites", "productNotifies", "inMessages", "outMessages", "pointLogs");
 		}
-		return "redirect:list";
+		return Message.success("操作成功");
 	}
 
 	/**
 	 * 列表
 	 */
 	@GetMapping("/list")
-	public String list(Pageable pageable, ModelMap model) {
-		model.addAttribute("memberRanks", memberRankService.findAll());
-		model.addAttribute("memberAttributes", memberAttributeService.findAll());
-		model.addAttribute("page", memberService.findPage(pageable));
-		return "admin/member/list";
+	public Page<Member> list(Pageable pageable) {
+		return memberService.findPage(pageable);
 	}
 
 	/**
 	 * 删除
 	 */
 	@PostMapping("/delete")
-	public @ResponseBody Message delete(Long[] ids) {
+	public Message delete(Long[] ids) {
 		if (ids != null) {
 			for (Long id : ids) {
 				Member member = memberService.find(id);
