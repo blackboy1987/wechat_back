0466bc0cf1ff41f334b7d160e3ce55c76b4836f3
xial@plian.net <a12345678>
2020-04-15 17:07:42 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/igomall/controller/admin/FileController.java
    #	src/main/java/com/igomall/controller/admin/course/CourseController.java
    #	src/main/java/com/igomall/controller/common/course/CourseController.java
    #	src/main/java/com/igomall/controller/member/IndexController.java
    #	src/main/java/com/igomall/entity/course/Course.java
    #	src/main/java/com/igomall/entity/course/Lesson.java
    #	src/main/java/com/igomall/service/FileService.java
    #	src/main/java/com/igomall/service/impl/FileServiceImpl.java
    #	src/main/java/com/igomall/service/impl/SearchServiceImpl.java
    #	src/main/java/com/igomall/util/SystemUtils.java

--- src/main/java/com/igomall/util/wechat/DataCubeUtils.java
+++ src/main/java/com/igomall/util/wechat/DataCubeUtils.java
@@ -2,8 +2,7 @@ package com.igomall.util.wechat;
 
 import com.igomall.util.Date8Utils;
 import com.igomall.util.JsonUtils;
-import com.igomall.util.wechat.response.datacube.DataCubeUserCumulateResponse;
-import com.igomall.util.wechat.response.datacube.DataCubeUserSummaryResponse;
+import com.igomall.util.wechat.response.datacube.*;
 
 import java.util.Date;
 import java.util.HashMap;
@@ -47,4 +46,101 @@ public final class DataCubeUtils {
 
         return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeUserCumulateResponse.class);
     }
+
+    /**
+     *获取图文群发每日数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeArticleSummaryResponse getarticlesummary(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getarticlesummary";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeArticleSummaryResponse.class);
+    }
+
+    /**
+     *获取图文群发总数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeArticleTotalResponse getarticletotal(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getarticletotal";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeArticleTotalResponse.class);
+    }
+
+    /**
+     *获取图文统计数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeUserReadResponse getuserread(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getuserread";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeUserReadResponse.class);
+    }
+
+    /**
+     *获取图文统计分时数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeUserReadHourResponse getuserreadhour(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getuserreadhour";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeUserReadHourResponse.class);
+    }
+
+    /**
+     *获取图文分享转发数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeUserShareResponse getusershare(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getusershare";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params), DataCubeUserShareResponse.class);
+    }
+
+    /**
+     *获取图文分享转发分时数据
+     * @param beginDate
+     *  获取数据的起始日期，begin_date和end_date的差值需小于“最大时间跨度”（比如最大时间跨度为1时，begin_date和end_date的差值只能为0，才能小于1），否则会报错
+     * @param endDate
+     *   获取数据的结束日期，end_date允许设置的最大值为昨日
+     */
+    public static DataCubeUserShareHourResponse getusersharehour(Date beginDate, Date endDate){
+        String url="https://api.weixin.qq.com/datacube/getusersharehour";
+        Map<String,Object> params = new HashMap<>();
+        params.put("begin_date", Date8Utils.formatDateToString(beginDate,"yyyy-MM-dd"));
+        params.put("end_date", Date8Utils.formatDateToString(endDate,"yyyy-MM-dd"));
+
+        return JsonUtils.toObject(WechatUtils.get(url,params),DataCubeUserShareHourResponse.class);
+    }
+
 }
