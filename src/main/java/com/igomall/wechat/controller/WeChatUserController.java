5f9f7b7a4dc93465217769a007cda7acc8f86051
1169794338@qq.com <Liziyi521521>
2020-04-18 00:04:50 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/igomall/controller/admin/FileController.java
    #	src/main/java/com/igomall/controller/admin/course/CourseController.java
    #	src/main/java/com/igomall/controller/common/course/CourseController.java
    #	src/main/java/com/igomall/controller/member/IndexController.java
    #	src/main/java/com/igomall/entity/course/Course.java
    #	src/main/java/com/igomall/entity/course/Lesson.java
    #	src/main/java/com/igomall/service/FileService.java
    #	src/main/java/com/igomall/service/impl/FileServiceImpl.java
    #	src/main/java/com/igomall/service/impl/SearchServiceImpl.java
    #	src/main/java/com/igomall/util/SystemUtils.java

--- src/main/java/com/igomall/wechat/controller/WeChatUserController.java
+++ src/main/java/com/igomall/wechat/controller/WeChatUserController.java
@@ -1,5 +1,6 @@
 package com.igomall.wechat.controller;
 
+import com.igomall.common.Order;
 import com.igomall.common.Page;
 import com.igomall.common.Pageable;
 import com.igomall.controller.admin.BaseController;
@@ -10,6 +11,7 @@ import com.igomall.wechat.util.response.user.UserGetResponse;
 import com.igomall.wechat.util.response.user.UserInfoBatchGetResponse;
 import com.igomall.wechat.util.response.user.UserInfoResponse;
 import com.igomall.wechat.util.response.user.UserInfoUpdateRemarkResponse;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,6 +31,11 @@ public class WeChatUserController extends BaseController {
 
     @PostMapping("/list")
     public Page<WeChatUser> list(Pageable pageable, Integer status, Date beginDate,Date endDate){
+        if(StringUtils.isEmpty(pageable.getOrderProperty()) || pageable.getOrderDirection()==null){
+            pageable.setOrderDirection(Order.Direction.desc);
+            pageable.setOrderProperty("subscribeTime");
+        }
+
         return wechatUserService.findPage(pageable,status,beginDate,endDate);
     }
 
