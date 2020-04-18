7c7073f536ad3cead741be6c11079783c2e6c660
xial@plian.net <a12345678>
2020-04-18 11:26:54 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/com/igomall/controller/admin/FileController.java
    #	src/main/java/com/igomall/controller/admin/course/CourseController.java
    #	src/main/java/com/igomall/controller/common/course/CourseController.java
    #	src/main/java/com/igomall/controller/member/IndexController.java
    #	src/main/java/com/igomall/entity/course/Course.java
    #	src/main/java/com/igomall/entity/course/Lesson.java
    #	src/main/java/com/igomall/service/FileService.java
    #	src/main/java/com/igomall/service/impl/FileServiceImpl.java
    #	src/main/java/com/igomall/service/impl/SearchServiceImpl.java
    #	src/main/java/com/igomall/util/SystemUtils.java

--- src/main/java/com/igomall/WeChatApplication.java
+++ src/main/java/com/igomall/WeChatApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class IgomallEduBackApplication {
+public class WeChatApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IgomallEduBackApplication.class, args);
+		SpringApplication.run(WeChatApplication.class, args);
 	}
 
 }
